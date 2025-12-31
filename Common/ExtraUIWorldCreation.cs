b2ed635a253f92152efcc954b7a40396047e2d82
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-12-31 05:44:51 -0800

    Cleanup Cleanup

--- Common/ExtraUIWorldCreation.cs
+++ Common/ExtraUIWorldCreation.cs
@@ -10,98 +10,43 @@ using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.UI;
 using Terraria.WorldBuilding;
+#pragma warning disable CS0414 // Field is assigned but its value is never used
+#pragma warning disable CS0169 // Field is never used
 
 namespace ExtraWorldSizes.Common;
 
 public static class ExtraUIWorldCreation
 {
+    #region Properties
     private static GroupOptionButton<WorldSizeID>[] _sizeButtons = Array.Empty<GroupOptionButton<WorldSizeID>>();
-    private static WorldSizeID _sizeChoice;
-    private static MethodInfo assignRandomWorldNameMethod;
-    private static MethodInfo assignRandomWorldSeedMethod;
-    private static MethodInfo updateInputFieldsMethod;
-    private static MethodInfo updatePreviewPlateMethod;
-
-    private static FieldInfo descriptionTextField;
-
+    private static WorldSizeID _sizeChoice = WorldSizeID.Medium;
+    #endregion
+    
+    #region Reflection Properties
+    private static MethodInfo _assignRandomWorldNameMethod;
+    private static MethodInfo _assignRandomWorldSeedMethod;
+    private static MethodInfo _updateInputFieldsMethod;
+    private static MethodInfo _updatePreviewPlateMethod;
+    
+    private static FieldInfo _descriptionTextField;
+    #endregion
+    
     public static void OnLoad()
     {
-        On_UIWorldCreation.SetDefaultOptions += OnSetDefaultOptions;
+        On_UIWorldCreation.SetDefaultOptions += On_SetDefaultOptions;
         On_UIWorldCreation.AddWorldSizeOptions += On_AddWorldSizeOptions;
-        IL_UIWorldCreation.FinishCreatingWorld += IlFinishCreationWorld;
-
+        IL_UIWorldCreation.FinishCreatingWorld += Il_FinishCreationWorld;
     }
 
     public static void OnUnload()
     {
-        On_UIWorldCreation.SetDefaultOptions -= OnSetDefaultOptions;
+        On_UIWorldCreation.SetDefaultOptions -= On_SetDefaultOptions;
         On_UIWorldCreation.AddWorldSizeOptions -= On_AddWorldSizeOptions;
-        IL_UIWorldCreation.FinishCreatingWorld -= IlFinishCreationWorld;
-    }
-    
-    private static void IlFinishCreationWorld(ILContext il)
-    {
-        var mod = ModContent.GetInstance<ExtraMain>();
-        
-        var ilCursor = new ILCursor(il);
-        if (ilCursor.TryGotoNext(MoveType.After, x => x.MatchCallOrCallvirt(typeof(WorldGen), nameof(WorldGen.setWorldSize))))
-        {
-            ilCursor.EmitDelegate(() =>
-            {
-                mod.Logger.Warn("Found");
-                switch (_sizeChoice)
-                {
-                    case WorldSizeID.Tiny:
-                        Main.maxTilesX = 2100;
-                        Main.maxTilesY = 600;
-                        break;
-                    case WorldSizeID.Small:
-                        Main.maxTilesX = 4200;
-                        Main.maxTilesY = 1200;
-                        break;
-                    case WorldSizeID.Medium:
-                        Main.maxTilesX = 6400;
-                        Main.maxTilesY = 1800;
-                        break;
-                    case WorldSizeID.Large:
-                        Main.maxTilesX = 8400;
-                        Main.maxTilesY = 2400;
-                        break;
-                    case WorldSizeID.Huge:
-                        Main.maxTilesX = 10500;
-                        Main.maxTilesY = 3000;
-                        break;
-                }
-                
-                WorldGen.setWorldSize();
-            });
-        }
-        else
-        {
-            throw new Exception();
-        }
-    }
-    
-    private static void AssignRandomWorldName(UIWorldCreation self)
-    {
-        assignRandomWorldNameMethod ??= typeof(UIWorldCreation).GetMethod("AssignRandomWorldName", BindingFlags.Instance | BindingFlags.NonPublic);
-        assignRandomWorldNameMethod?.Invoke(self, Array.Empty<object>());
-    }
-    
-    private static void AssignRandomWorldSeed(UIWorldCreation self)
-    {
-        assignRandomWorldSeedMethod ??= typeof(UIWorldCreation).GetMethod("AssignRandomWorldSeed", BindingFlags.Instance | BindingFlags.NonPublic);
-        assignRandomWorldSeedMethod?.Invoke(self, Array.Empty<object>());
-    }
-    
-    private static void UpdateInputFields(UIWorldCreation self)
-    {
-        updateInputFieldsMethod ??= typeof(UIWorldCreation).GetMethod("UpdateInputFields", BindingFlags.Instance | BindingFlags.NonPublic);
-        updateInputFieldsMethod?.Invoke(self, Array.Empty<object>());
+        IL_UIWorldCreation.FinishCreatingWorld -= Il_FinishCreationWorld;
     }
 
-
-    private static void OnSetDefaultOptions(On_UIWorldCreation.orig_SetDefaultOptions orig, UIWorldCreation self)
+    #region Hooks
+    private static void On_SetDefaultOptions(On_UIWorldCreation.orig_SetDefaultOptions orig, UIWorldCreation self)
     {
         AssignRandomWorldName(self);
         AssignRandomWorldSeed(self);
@@ -110,47 +55,30 @@ public static class ExtraUIWorldCreation
         _sizeChoice = ModContent.GetInstance<ExtraConfig>().DefaultWorldSize;
         foreach (var button in _sizeButtons) button.SetCurrentOption(_sizeChoice);
 
-        var difficultyButtons = (Array)typeof(UIWorldCreation)
-            .GetField("_difficultyButtons", BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(self);
+        var difficultyButtons = (Array)typeof(UIWorldCreation).GetField("_difficultyButtons", BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(self);
         if (difficultyButtons != null)
         {
             foreach (var button in difficultyButtons)
             {
                 if (button == null) continue;
-
-                Type enumType = button.GetType().GenericTypeArguments[0];
-                int rawValue = 0;
-
-                object boxedEnum = Enum.ToObject(enumType, rawValue);
-
-                MethodInfo m = button.GetType().GetMethod("SetCurrentOption",
-                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-                m.Invoke(button, new[] { boxedEnum });
+                
+                var methodInfo = button.GetType().GetMethod("SetCurrentOption", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                methodInfo?.Invoke(button, new[] { Enum.ToObject(button.GetType().GenericTypeArguments[0], 0) });
             }
         }
 
-        var evilButtons = (Array)typeof(UIWorldCreation)
-            .GetField("_evilButtons", BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(self);
+        var evilButtons = (Array)typeof(UIWorldCreation).GetField("_evilButtons", BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(self);
         if (evilButtons != null)
         {
             foreach (var button in evilButtons)
             {
                 if (button == null) continue;
-
-                Type enumType = button.GetType().GenericTypeArguments[0];
-                int rawValue = 0;
-
-                object boxedEnum = Enum.ToObject(enumType, rawValue);
-
-                MethodInfo m = button.GetType().GetMethod("SetCurrentOption",
-                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-                m.Invoke(button, new[] { boxedEnum });
+                
+                var methodInfo = button.GetType().GetMethod("SetCurrentOption", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                methodInfo?.Invoke(button, new[] { Enum.ToObject(button.GetType().GenericTypeArguments[0], 0) });
             }
         }
     }
-
     private static void On_AddWorldSizeOptions(On_UIWorldCreation.orig_AddWorldSizeOptions orig, UIWorldCreation self, UIElement container, float accumulatedHeight, UIElement.MouseEvent clickEvent, string tagGroup, float usableWidthPercent)
     {
         var worldSizeIDs = new WorldSizeID[] {
@@ -162,6 +90,7 @@ public static class ExtraUIWorldCreation
         };
 
         var mod = ModContent.GetInstance<ExtraMain>();
+        if (mod == null) return;
         
         var titles = new LocalizedText[] {
             Language.GetOrRegister(mod.GetLocalizationKey("UI.WorldSizeTiny"), () => "Tiny"),
@@ -216,65 +145,101 @@ public static class ExtraUIWorldCreation
             groupOptionButton.Top.Set(accumulatedHeight, 0f);
             groupOptionButton.SetSnapPoint(tagGroup, i);
             
-            groupOptionButton.OnLeftMouseDown += (evt, element) =>
-            {
-                ClickSizeOption(self, evt, element);
-            };
-            
-            groupOptionButton.OnMouseOver += (evt, element) =>
-            {
-                ShowOptionDescription(self, evt, element);
-            };
-
-            groupOptionButton.OnMouseOut += (evt, element) =>
-            {
-                ClearOptionDescription(self, evt, element);
-            };
+            groupOptionButton.OnLeftMouseDown += (_, element) => ClickSizeOption(self, element);
+            groupOptionButton.OnMouseOver += (_, element) => ShowOptionDescription(self, element);
+            groupOptionButton.OnMouseOut += (_, _) => ClearOptionDescription(self);
             
             container.Append(groupOptionButton);
             
             _sizeButtons[i] = groupOptionButton;
         }
     }
-    
-    private static void ClickSizeOption(UIWorldCreation self, UIMouseEvent evt, UIElement listeningElement)
+    private static void Il_FinishCreationWorld(ILContext il)
     {
-        var groupOptionButton = (GroupOptionButton<WorldSizeID>)listeningElement;
+        var mod = ModContent.GetInstance<ExtraMain>();
+        if (mod == null) return;
         
-        _sizeChoice = groupOptionButton.OptionValue;
-        foreach (var button in _sizeButtons) button.SetCurrentOption(groupOptionButton.OptionValue);
+        var ilCursor = new ILCursor(il);
+        if (!ilCursor.TryGotoNext(MoveType.After, x => x.MatchCallOrCallvirt(typeof(WorldGen), nameof(WorldGen.setWorldSize))))
+        {
+            mod.Logger.Error($"Failed to find Instruction for {nameof(Il_FinishCreationWorld)}");
+            return;
+        }
+        
+        ilCursor.EmitDelegate(() =>
+        {
+            switch (_sizeChoice)
+            {
+                case WorldSizeID.Tiny:
+                    Main.maxTilesX = ExtraWorldGen.WorldSizeTinyX;
+                    Main.maxTilesY = ExtraWorldGen.WorldSizeTinyY;
+                    WorldGen.setWorldSize();
+                    break;
+                case WorldSizeID.Huge:
+                    Main.maxTilesX = ExtraWorldGen.WorldSizeHugeX;
+                    Main.maxTilesY = ExtraWorldGen.WorldSizeHugeY;
+                    WorldGen.setWorldSize();
+                    break;
+                case WorldSizeID.Small:
+                case WorldSizeID.Medium:
+                case WorldSizeID.Large: 
+                default: break;
+            }
+        });
+    }
+    #endregion
 
-        UpdatePreviewPlate(self);
+    #region Helpers
+    private static void AssignRandomWorldName(UIWorldCreation self)
+    {
+        _assignRandomWorldNameMethod ??= typeof(UIWorldCreation).GetMethod("AssignRandomWorldName", BindingFlags.Instance | BindingFlags.NonPublic);
+        _assignRandomWorldNameMethod?.Invoke(self, Array.Empty<object>());
     }
     
-    private static void ClearOptionDescription(UIWorldCreation self, UIMouseEvent evt, UIElement listeningElement)
+    private static void AssignRandomWorldSeed(UIWorldCreation self)
     {
-        descriptionTextField ??= typeof(UIWorldCreation).GetField("_descriptionText", BindingFlags.Instance | BindingFlags.NonPublic);
-        (descriptionTextField?.GetValue(self) as UIText)?.SetText(Language.GetText("UI.WorldDescriptionDefault"));
+        _assignRandomWorldSeedMethod ??= typeof(UIWorldCreation).GetMethod("AssignRandomWorldSeed", BindingFlags.Instance | BindingFlags.NonPublic);
+        _assignRandomWorldSeedMethod?.Invoke(self, Array.Empty<object>());
+    }
+    
+    private static void UpdateInputFields(UIWorldCreation self)
+    {
+        _updateInputFieldsMethod ??= typeof(UIWorldCreation).GetMethod("UpdateInputFields", BindingFlags.Instance | BindingFlags.NonPublic);
+        _updateInputFieldsMethod?.Invoke(self, Array.Empty<object>());
+    }
+    
+    private static void ClearOptionDescription(UIWorldCreation self)
+    {
+        _descriptionTextField ??= typeof(UIWorldCreation).GetField("_descriptionText", BindingFlags.Instance | BindingFlags.NonPublic);
+        (_descriptionTextField?.GetValue(self) as UIText)?.SetText(Language.GetText("UI.WorldDescriptionDefault"));
     }
     
     private static void UpdatePreviewPlate(UIWorldCreation self)
     {
-        updatePreviewPlateMethod ??= typeof(UIWorldCreation).GetMethod("UpdatePreviewPlate", BindingFlags.Instance | BindingFlags.NonPublic);
-        updatePreviewPlateMethod?.Invoke(self, Array.Empty<object>());
+        _updatePreviewPlateMethod ??= typeof(UIWorldCreation).GetMethod("UpdatePreviewPlate", BindingFlags.Instance | BindingFlags.NonPublic);
+        _updatePreviewPlateMethod?.Invoke(self, Array.Empty<object>());
     }	
-
-    private static void ShowOptionDescription(UIWorldCreation self, UIMouseEvent evt, UIElement listeningElement)
+    
+    private static void ClickSizeOption(UIWorldCreation self, UIElement listeningElement)
     {
-        var localizedText = listeningElement switch
-        {
-            GroupOptionButton<WorldSizeID> button => button.Description,
-            //GroupOptionButton<WorldDifficultyID> button => button.Description,
-            //GroupOptionButton<WorldEvilID> button => button.Description,
-            UICharacterNameButton button => button.Description,
-            GroupOptionButton<bool> button => button.Description,
-            _ => null
-        };
+        var groupOptionButton = listeningElement as GroupOptionButton<WorldSizeID>;
+        if (groupOptionButton == null) return;
+        
+        _sizeChoice = groupOptionButton.OptionValue;
+        
+        foreach (var button in _sizeButtons)
+            button.SetCurrentOption(groupOptionButton.OptionValue);
 
-        if (localizedText != null)
-        {
-            descriptionTextField ??= typeof(UIWorldCreation).GetField("_descriptionText", BindingFlags.Instance | BindingFlags.NonPublic);
-            (descriptionTextField?.GetValue(self) as UIText)?.SetText(localizedText);
-        }
+        UpdatePreviewPlate(self);
     }
+    
+    private static void ShowOptionDescription(UIWorldCreation self, UIElement listeningElement)
+    {
+        var localizedText = (listeningElement as GroupOptionButton<WorldSizeID>)?.Description;
+        if (localizedText == null) return;
+        
+        _descriptionTextField ??= typeof(UIWorldCreation).GetField("_descriptionText", BindingFlags.Instance | BindingFlags.NonPublic);
+        (_descriptionTextField?.GetValue(self) as UIText)?.SetText(localizedText);
+    }
+    #endregion
 }
