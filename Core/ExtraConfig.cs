2967c774d081ee95634ec663148a3c912a96c1cd
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-12-31 04:39:01 -0800

    Setup Config

--- Core/ExtraConfig.cs
+++ Core/ExtraConfig.cs
@@ -1,8 +1,12 @@
-﻿using Terraria.ModLoader.Config;
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
 
 namespace ExtraWorldSizes.Core;
 
 public sealed class ExtraConfig : ModConfig
 {
     public override ConfigScope Mode => ConfigScope.ClientSide;
+    
+    [DefaultValue(true)] public bool EnableTinyWorldSize { get; set; }
+    [DefaultValue(true)] public bool EnableHugeWorldSize { get; set; }
 }
